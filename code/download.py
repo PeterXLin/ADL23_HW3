b614241e0eaef54c672065b63a8a70eaced7abce
Peter <cspeter@g.ncu.edu.tw>
2023-11-15 14:17:05 +0800

    refact: download dataset and code provided by TA

--- code/download.py
+++ code/download.py
@@ -5,12 +5,11 @@ import gdown
 # gdown.download_folder(url, quiet=True, use_cookies=False)
 
 # download hw3.zip
-url = "https://drive.google.com/file/d/1n0EY5c7nyzz_fhRdmAJJVojAM9GDUrZO/view?usp=drive_link"
-output = "hw3.zip"
-gdown.download(url, output, quiet=False)
+url = "https://drive.google.com/file/d/1n0EY5c7nyzz_fhRdmAJJVojAM9GDUrZO/view?usp=sharing"
+output = "./hw3.zip"
+gdown.download(url, output, quiet=False, fuzzy=True)
 
-
-# download Taiwan-LLM-7B-v2.0-chat.zip
-url = "https://drive.google.com/file/d/1OkteUnpFGQvAU79j0OY-j2jo5J8mL_tp/view?usp=drive_link"
-output = "Taiwan-LLM-7B-v2.0-chat.zip"
-gdown.download(url, output, quiet=False)
+# # download Taiwan-LLM-7B-v2.0-chat.zip
+# url = "https://drive.google.com/file/d/1OkteUnpFGQvAU79j0OY-j2jo5J8mL_tp/view?usp=sharing"
+# output = "Taiwan-LLM-7B-v2.0-chat.zip"
+# gdown.download(url, output, quiet=False, fuzzy=True)
--- code/unzip.py
+++ code/unzip.py
@@ -1,4 +1,4 @@
 import zipfile
 
-with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-    zip_ref.extractall(directory_to_extract_to)
+with zipfile.ZipFile("./hw3.zip", 'r') as zip_ref:
+    zip_ref.extractall("./hw3")
