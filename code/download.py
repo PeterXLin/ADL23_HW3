c61810380a613e21cc0b09bcbb068e5fb8262033
Peter <cspeter@g.ncu.edu.tw>
2023-11-15 13:47:25 +0800

    feat: implement download dataset & model checkpoint code

---
+++ code/download.py
@@ -0,0 +1,16 @@
+import gdown
+
+# # download hw3.zip
+# url = "https://drive.google.com/file/d/1n0EY5c7nyzz_fhRdmAJJVojAM9GDUrZO/view?usp=drive_link"
+# gdown.download_folder(url, quiet=True, use_cookies=False)
+
+# download hw3.zip
+url = "https://drive.google.com/file/d/1n0EY5c7nyzz_fhRdmAJJVojAM9GDUrZO/view?usp=drive_link"
+output = "hw3.zip"
+gdown.download(url, output, quiet=False)
+
+
+# download Taiwan-LLM-7B-v2.0-chat.zip
+url = "https://drive.google.com/file/d/1OkteUnpFGQvAU79j0OY-j2jo5J8mL_tp/view?usp=drive_link"
+output = "Taiwan-LLM-7B-v2.0-chat.zip"
+gdown.download(url, output, quiet=False)
---
+++ code/unzip.py
@@ -0,0 +1,4 @@
+import zipfile
+
+with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+    zip_ref.extractall(directory_to_extract_to)
