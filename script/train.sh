539d537bb42ec9cd318abf41d1c52c733d063abd
Peter Lin <petercshsiang@gmail.com>
2023-11-18 13:00:47 +0000

    feat: add exploratory data analysis code

--- script/train.sh
+++ script/train.sh
@@ -5,6 +5,7 @@ python ./code/sft.py \
     --model_path "./checkpoint/Taiwan-LLM-7B-v2.0-chat" \
     --train_path "./data/train.json" \
     --output_dir "./checkpoint/finetune_1" \
+    --seq_length 640 \
     --num_train_epochs 2 \
     --use_peft True \
     --batch_size 4 \
