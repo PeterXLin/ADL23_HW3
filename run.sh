71926d0068e1ca74c57cd64a860979483d9ad733
Peter Lin <petercshsiang@gmail.com>
2023-11-27 06:40:30 +0000

    bug: modify run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ peft_model=$2
 input=$3
 output=$4
 
-export CUDA_VISIBLE_DEVICES=1 && python ./inference.py --base_model_name_or_path $base_model --peft_model_path $peft_model --input_path $input --output_path $output
+python ./inference.py --base_model_name_or_path $base_model --peft_model_path $peft_model --input_path $input --output_path $output
